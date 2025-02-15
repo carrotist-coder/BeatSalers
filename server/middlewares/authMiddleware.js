b22eba6bbcd6e5ecc71ae9d0b181e07d1a9d3f30
Carrotist <romark99@gmail.com>
2025-02-15 23:06:49 +0300

    Created auth middleware

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (role) {
+    return function (req, res, next) {
+        if (req.method === 'OPTIONS') {
+            next();
+            return;
+        }
+
+        try {
+            const authHeader = req.headers.authorization;
+
+            if (!authHeader) {
+                return res.status(401).json({ message: 'Не авторизован' });
+            }
+
+            const token = authHeader.split(' ')[1];
+            if (!token) {
+                return res.status(401).json({ message: 'Некорректный токен' });
+            }
+
+            const decoded = jwt.verify(token, process.env.SECRET_KEY);
+            req.user = decoded;
+
+            if (role && decoded.role !== role) {
+                return res.status(403).json({ message: 'Нет доступа' });
+            }
+
+            next();
+        } catch (e) {
+            res.status(401).json({ message: 'Не авторизован' });
+        }
+    };
+};
