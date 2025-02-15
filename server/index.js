ea0c2b5c4659e9b1cd12e1bdeb3a0f87f95c642c
Carrotist <romark99@gmail.com>
2025-02-15 12:13:23 +0300

    Created basic server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
