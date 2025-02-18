0687b8ebb9d17bde4184237b1c9445c6dc4ff7c4
Carrotist <romark99@gmail.com>
2025-02-18 13:58:37 +0300

    Created basic client React app

---
+++ client/src/index.js
@@ -0,0 +1,15 @@
+import React, { createContext } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+export const Context = createContext(null);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
