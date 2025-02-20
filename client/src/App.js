4d3059b3ee8846968ea0d4ee5daabd17390d5624
Carrotist <romark99@gmail.com>
2025-02-20 11:58:14 +0300

    Added automatic user authentication check on app load

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import Auth from "./pages/Auth";
+import {Spinner} from "react-bootstrap";
+import {observer} from "mobx-react-lite";
+import {check} from "./http/UserAPI";
+import {Context} from "./index";
 
-function App() {
+const App = observer(() => {
+    const {user} = useContext(Context);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const initialize = async () => {
+            try {
+                const data = await check();
+                user.setUser(data);
+                user.setIsAuth(true);
+            } catch (error) {
+                if (error.response && error.response.status === 401) {
+                    console.warn('Пользователь не авторизован');
+                    user.setUser({});
+                    user.setIsAuth(false);
+                } else {
+                    console.error('Ошибка при проверке пользователя:', error.message);
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        initialize();
+    }, [user]);
+
+    if (loading) {
+        return <Spinner animation={"grow"}/>
+    }
+
     return (
         <Auth />
     );
-}
+});
 
 export default App;
