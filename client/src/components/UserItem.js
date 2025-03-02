61eba5b0f6898f51c2fc759a5de6a7a7e355406c
Carrotist <romark99@gmail.com>
2025-03-02 23:56:14 +0300

    Updated User Item: linked to PROFILE_ROUTE

--- client/src/components/UserItem.js
+++ client/src/components/UserItem.js
@@ -1,12 +1,18 @@
 import React from "react";
-import {CardGroup} from "react-bootstrap";
-import Card from 'react-bootstrap/Card';
+import { CardGroup, Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import "./UserStyles.css";
+import { PROFILE_ROUTE } from "../utils/consts";
 
 function UserItem() {
+    const navigate = useNavigate();
+    const handleCardClick = () => {
+        navigate(PROFILE_ROUTE);
+    };
+
     return (
-        <CardGroup className="user-item">
-            <Card>
+        <CardGroup className="user-item" onClick={handleCardClick}>
+            <Card style={{ cursor: "pointer" }}>
                 <Card.Img variant="top" src="https://dummyimage.com/300x300" />
                 <Card.Body>
                     <Card.Title>Имя музыканта</Card.Title>
