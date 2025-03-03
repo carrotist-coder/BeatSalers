1825163bacd63878c6576530f1d28e81f130fe5d
Carrotist <romark99@gmail.com>
2025-03-03 17:37:41 +0300

    Route name changed, minor changes

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -2,6 +2,6 @@ export const MAIN_ROUTE = '/'; // Главная страница
 export const AUTH_ROUTE = '/auth'; // Страница авторизации/регистрации
 export const USERS_ROUTE = '/users'; // Страница пользователей
 export const PROFILE_ROUTE = '/profiles/:username'; // Страница деталей конкретного пользователя
-export const MY_PROFILE_ROUTE = '/profiles/me'; // Страница текущего профиля
+export const MY_PROFILE_ROUTE = '/me'; // Страница текущего профиля
 export const BEATS_ROUTE = '/beats'; // Страница битов
 export const BEAT_DETAILS_ROUTE = '/beats/:id'; // Страница деталей бита
